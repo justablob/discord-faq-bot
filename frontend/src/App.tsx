10c1c80a18aaee552ebcac59fb4b7d26242edf76
justablob <me@blob.dev>
2022-05-02 11:34:17 +0200

    add frontend dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button } from '@mantine/core'
 
 function App() {
   const [count, setCount] = useState(0)
